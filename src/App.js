5b5b49de198b8748b5649e3a87ff7a8df0dbb8b1
vandana <vandanaprathapar@gmail.com>
2024-06-22 10:09:32 +1000

    added margins to make page flexible

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ function App() {
           It's Me! Vandana
         </h1>
         <div className="Incoming">
-        <p>
-          Woohooo! Got the react app deployed :) 
-          <br />
-          <br />
-          Hi! I'm Vandana. I'm early into my career in technology and am using this space to document my journey as it goes. I chose to make a react app for this purpose because I also want to develop my frontend skills a bit more and use this site to talk about all things ✨ Me ✨. 
-          <br />
-          <br />
-          More updates to come!
-
-        </p>
+          <p>
+            Woohooo! Got the react app deployed :) 
+            <br />
+            <br />
+            Hi! I'm Vandana. I'm early into my career in technology and am using this space to document my journey as it goes. I chose to make a react app for this purpose because I also want to develop my frontend skills a bit more and use this site to talk about all things ✨ Me ✨. 
+            <br />
+            <br />
+            More updates to come!
+          </p>
         </div>
         
     </div>
