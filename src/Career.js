531504657da3266ee7d83bc7371e50b7accc250d
vandana <vandanaprathapar@gmail.com>
2024-11-24 21:06:17 +1100

    make nav more obviously nav and adjusting bordering and fixed typo

--- src/Career.js
+++ src/Career.js
@@ -9,39 +9,39 @@ function Career() {
             I apologise that this currently looks like my resume but I'm throwing in some key points about my career before I begin to embellish!
             <br></br><br></br>
             <div className="withincoming">
-            <b>HSC 2019</b>
-            <p> 
-                Finishing High school and unsure what my future would look like, I only knew two things: I liked science (especially chemistry) and 
-                I wanted to do some coding subjects since coding seemed like it was going to be pretty popular and could be useful wherever I go.
-            </p>
-            <b>Undergraduate 2020 - 2022</b>
-            <p>
-                I started the degree thinking I was going to be a researcher working in a lab. I never understood programming at the beginning and started to feel overwhelmed in my first and second year. 
-                However, during the second lockdown I started tampering around with HMTL, CSS and JavaScript on my own to make a website about knitting and crochet. 
-                With a lot of free time then, I wanted to show more tutorials on how to knit and crochet for people who might be interested. <br/><br/>
-                After all the lockdowns, I took on opportunities to participate in societies such as UNSW Bioinformatics Society (BINFSOC) acting as an events executive in BINFSOC to bring small cohort together through professional and social events. 
-            </p>
-            <b>Undergraduate 2022/23 Summer</b>
-            <p>
-                I did an internship in the engineering stream at CBA; working in the payments area, doing development and maintenance for a Terminal Management System, used to track the terminals htat were purchased by merchants from the business bank. 
-            </p>
-            <b>Final Year of Undergrad 2023</b>
-            <p>
-                I completed my Honours on “Refactoring and Further Development of the Multilevel Genome Typing Database”, which is a website for researchers to perform the MGT method on bacterial genomes to help identify relationships between many collected isolates. 
-                My project invovled refactoring the codebase of the Django application to improve feature building, and optimising the local installation of the application for wet lab researchers running the method and app on their local machines. <br/><br/>
-                Towards the end of my internship I also completed an internship at a medical research institute to build a database and UI for wetlab researchers to track their experiment samples as they go through multiple processes and tracking each process in a structured way. 
-            </p>
-            <b>2024</b>
-            <p>
-                I have returned to the bank I did my summer internship at to complete a graduate program. <br/>
-                My first rotation was working in the HR function; building and maintaining platforms that support over 48 900 employees. My biggest achievement was building a GitHub Action that can be used for Integration Testing in Workday Extend Applications. <br/>
-                Currently working in a team that works on the accounts and transactions pages in the retail app. I work on the backend side of these applications; building and maintaining APIs that control information displayed to millions of customers. <br/>
-            </p>
-            <b>The future</b>
-            <p>
-                The job market is fickle and I'm not fully sure what the future will hold for me. However, the areas I really want to develop on at the moment are in security practices when it comes to writing code, and understanding cloud. 
-                I also want to develop further on understanding complex architectures when it comes to building APIs for complex applications and bigger systems. 
-            </p>
+                <b>HSC 2019</b>
+                <p> 
+                    Finishing High school and unsure what my future would look like, I only knew two things: I liked science (especially chemistry) and 
+                    I wanted to do some coding subjects since coding seemed like it was going to be pretty popular and could be useful wherever I go.
+                </p>
+                <b>Undergraduate 2020 - 2022</b>
+                <p>
+                    I started the degree thinking I was going to be a researcher working in a lab. I never understood programming at the beginning and started to feel overwhelmed in my first and second year. 
+                    However, during the second lockdown I started tampering around with HMTL, CSS and JavaScript on my own to make a website about knitting and crochet. 
+                    With a lot of free time then, I wanted to show more tutorials on how to knit and crochet for people who might be interested. <br/><br/>
+                    After all the lockdowns, I took on opportunities to participate in societies such as UNSW Bioinformatics Society (BINFSOC) acting as an events executive in BINFSOC to bring small cohort together through professional and social events. 
+                </p>
+                <b>Undergraduate 2022/23 Summer</b>
+                <p>
+                    I did an internship in the engineering stream at a prominent Australian bank; working in the payments area, doing development and maintenance for a Terminal Management System, used to track the terminals that were purchased by merchants from the business bank. 
+                </p>
+                <b>Final Year of Undergrad 2023</b>
+                <p>
+                    I completed my Honours on “Refactoring and Further Development of the Multilevel Genome Typing Database”, which is a website for researchers to perform the MGT method on bacterial genomes to help identify relationships between many collected isolates. 
+                    My project invovled refactoring the codebase of the Django application to improve feature building, and optimising the local installation of the application for wet lab researchers running the method and app on their local machines. <br/><br/>
+                    Towards the end of my internship I also completed an internship at a medical research institute to build a database and UI for wetlab researchers to track their experiment samples as they go through multiple processes and tracking each process in a structured way. 
+                </p>
+                <b>2024</b>
+                <p>
+                    I have returned to the bank I did my summer internship at to complete a graduate program.
+                    My first rotation was working in the HR function; building and maintaining platforms that support over 48 900 employees. My biggest achievement was building a GitHub Action that can be used for Integration Testing in Workday Extend Applications. <br/>
+                    Currently working in a team that works on the accounts and transactions pages in the retail app. I work on the backend side of these applications; building and maintaining APIs that control information displayed to millions of customers. <br/>
+                </p>
+                <b>The future</b>
+                <p>
+                    The job market is fickle and I'm not fully sure what the future will hold for me. However, the areas I really want to develop on at the moment are in security practices when it comes to writing code, and understanding cloud. 
+                    I also want to develop further on understanding complex architectures when it comes to building APIs for complex applications and bigger systems. 
+                </p>
             </div>
             
           </div>
