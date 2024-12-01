8f93f72af28c1c7f3c00127cd14ef9f876c1dd16
vandana <vandanaprathapar@gmail.com>
2024-12-01 12:03:13 +1100

    implemented timeline with information successfully

--- src/Career.js
+++ src/Career.js
@@ -7,45 +7,10 @@ function Career() {
             <h2>
                My Career 
             </h2>
-            I apologise that this currently looks like my resume but I'm throwing in some key points about my career before I begin to embellish!
+            This timeline of my career was made using the Material UI framework with a couple customisations here and there to fit the colour scheme of this site. It's a work in progress, but that's the whole point of this website! 
+            I hope to also include more detail into my experiences but if I'm lucky and work hard, this page will always be an unfinished novel 😊. 
             <br></br><br></br>
             <VerticalLinearStepper></VerticalLinearStepper>
-            <div className="withincoming">
-                <b>HSC 2019</b>
-                <p> 
-                    Finishing High school and unsure what my future would look like, I only knew two things: I liked science (especially chemistry) and 
-                    I wanted to do some coding subjects since coding seemed like it was going to be pretty popular and could be useful wherever I go.
-                </p>
-                <b>Undergraduate 2020 - 2022</b>
-                <p>
-                    I started the degree thinking I was going to be a researcher working in a lab. I never understood programming at the beginning and started to feel overwhelmed in my first and second year. 
-                    However, during the second lockdown I started tampering around with HMTL, CSS and JavaScript on my own to make a website about knitting and crochet. 
-                    With a lot of free time then, I wanted to show more tutorials on how to knit and crochet for people who might be interested. <br/><br/>
-                    After all the lockdowns, I took on opportunities to participate in societies such as UNSW Bioinformatics Society (BINFSOC) acting as an events executive in BINFSOC to bring small cohort together through professional and social events. 
-                </p>
-                <b>Undergraduate 2022/23 Summer</b>
-                <p>
-                    I did an internship in the engineering stream at a prominent Australian bank; working in the payments area, doing development and maintenance for a Terminal Management System, used to track the terminals that were purchased by merchants from the business bank. 
-                </p>
-                <b>Final Year of Undergrad 2023</b>
-                <p>
-                    I completed my Honours on “Refactoring and Further Development of the Multilevel Genome Typing Database”, which is a website for researchers to perform the MGT method on bacterial genomes to help identify relationships between many collected isolates. 
-                    My project invovled refactoring the codebase of the Django application to improve feature building, and optimising the local installation of the application for wet lab researchers running the method and app on their local machines. <br/><br/>
-                    Towards the end of my internship I also completed an internship at a medical research institute to build a database and UI for wetlab researchers to track their experiment samples as they go through multiple processes and tracking each process in a structured way. 
-                </p>
-                <b>2024</b>
-                <p>
-                    I have returned to the bank I did my summer internship at to complete a graduate program.
-                    My first rotation was working in the HR function; building and maintaining platforms that support over 48 900 employees. My biggest achievement was building a GitHub Action that can be used for Integration Testing in Workday Extend Applications. <br/>
-                    Currently working in a team that works on the accounts and transactions pages in the retail app. I work on the backend side of these applications; building and maintaining APIs that control information displayed to millions of customers. <br/>
-                </p>
-                <b>The future</b>
-                <p>
-                    The areas I really want to develop on at the moment are in security practices when it comes to writing code, and understanding cloud. 
-                    I also want to develop further on understanding complex architectures when it comes to building APIs for complex applications and bigger systems. 
-                </p>
-            </div>
-            
           </div>
     );
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,27 +5,46 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
 import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
 const steps = [
   {
-    label: 'Select campaign settings',
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    label: 'High School Certificate: 2019',
+    description: `Finishing High school and unsure what my future would look like, I only knew two things: I liked science (especially chemistry) and 
+                  I wanted to do some coding subjects since coding seemed like it was going to be pretty popular and could be useful wherever I go.`,
   },
   {
-    label: 'Create an ad group',
-    description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
+    label: 'Undergraduate: 2020 - 2022',
+    description: `I started the degree thinking I was going to be a researcher working in a lab. I never understood programming at the beginning and started to feel overwhelmed in my first and second year. 
+                  However, during the second lockdown I started tampering around with HMTL, CSS and JavaScript on my own to make a website about knitting and crochet. 
+                  With a lot of free time then, I wanted to show more tutorials on how to knit and crochet for people who might be interested. 
+                  After all the lockdowns, I took on opportunities to participate in societies such as UNSW Bioinformatics Society (BINFSOC) 
+                  acting as an events executive in BINFSOC to bring small cohort together through professional and social events.`,
   },
   {
-    label: 'Create an ad',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: 'Undergraduate: 2022/23 Summer',
+    description: `I did an internship in the engineering stream at an Australian bank; working in the payments area. 
+                  I worked in a team to build and maintain the Terminal Management System, a website used by our customer 
+                  service representatives to track the terminals that were purchased by other businesses.`,
+  },
+  {
+    label: 'Final Year of Undergrad: 2023',
+    description: `I completed my Honours on “Refactoring and Further Development of the Multilevel Genome Typing Database”, which is a website for researchers to perform the MGT method on bacterial genomes 
+                  to help identify relationships between many collected isolates. My project invovled refactoring the codebase of the Django application to improve feature building, and optimising the local installation 
+                  of the application for wet lab researchers running the method and app on their local machines. Towards the end of my internship I also completed an internship at a medical research institute to 
+                  build a database and UI for wetlab researchers to track their experiment samples as they go through multiple processes and tracking each process in a structured way.`,
+  },
+  {
+    label: 'Being a Working Professional: 2024',
+    description: `I have returned to the bank I did my summer internship at to complete a graduate program. My first rotation was working in the HR function; building and maintaining platforms that support over 48 900 employees. 
+                  My biggest achievement was building a GitHub Action that can be used for Integration Testing in Workday Extend Applications.
+                  Currently working in a team that works on the accounts and transactions pages in the retail app. I work on the backend side of these applications; building and maintaining APIs that control the accounts and 
+                  balances information displayed to millions of customers.`,
+  },
+  {
+    label: 'The Future',
+    description: `At the moment, I'm really intrigued by becoming a more secure programmer and integrating proper security practices in software engineering, as well as understanding cloud concepts more generally. To work on these things, I'm assisting with a graduate run Capture the Flag Competition and working towards becoming an AppSec Champion.
+                  Additionally, I also want to develop further on understanding complex APIs and their flows. Most assignments in university are at a very small scale and typically programmed from scratch and joining my current team is helping me understand how improvements made to old API flows can improve our customers' experiences.`
   },
 ];
 
@@ -40,38 +59,35 @@ function VerticalLinearStepper() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
-    <Box sx={{ maxWidth: 400 }}>
+    <Box> 
       <Stepper activeStep={activeStep} orientation="vertical" sx={{
-          '& .MuiStepContent-root': {
-            borderRadius: '1px',
+          '& .MuiStepContent-root': { // THIS ENTIRE SECTION NEEDS TO BE REFACTORED!!!
             borderLeft: '1px solid black',
           },
           '& .MuiStepConnector-line': {
-            borderRadius: '1px',
             borderLeft: '1px solid black',
           },
           '& .MuiButton-containedPrimary': {
             backgroundColor: 'black',
             border: '1px',
+            '&:hover': {
+              backgroundColor: '#3c434a', 
+            },
           },
           '& .MuiButton-textPrimary': {
             color: 'black',
             border: '1px',
           },
           '& .MuiStepIcon-root.Mui-active': {
-            color: 'black', // Ensure active step icon remains black
+            color: 'black', 
           },
           '& .MuiStepIcon-root.Mui-completed': {
-            color: 'black', // Ensure active step icon remains black
+            color: 'black', 
           },
           '& .MuiSvgIcon-root': {
             borderRadius: '1px',
-            color: 'black', // Ensure all step icons remain black
+            color: 'black', 
           },
         }}>
         {steps.map((step, index) => (
@@ -79,30 +95,31 @@ function VerticalLinearStepper() {
             <StepLabel
               sx={{
                 '& .MuiStepContent-root': {
-                  borderRadius: '1px',
                   borderLeft: '1px solid black',
                 },
                 '& .MuiStepConnector-line': {
-                  borderRadius: '1px',
                   borderLeft: '1px solid black',
                 },
                 '& .MuiButton-containedPrimary': {
                   backgroundColor: 'black',
                   border: '1px',
+                  '&:hover': {
+                    backgroundColor: '#3c434a', 
+                  },
                 },
                 '& .MuiButton-textPrimary': {
                   color: 'black',
                   border: '1px',
-                },
+                },  
                 '& .MuiStepIcon-root.Mui-active': {
-                  color: 'black', // Ensure active step icon remains black
+                  color: 'black', 
                 },
                 '& .MuiStepIcon-root.Mui-completed': {
-                  color: 'black', // Ensure active step icon remains black
+                  color: 'black',
                 },   
                 '& .MuiSvgIcon-root': {
                   borderRadius: '1px',
-                  color: 'black', // Ensure all step icons remain black
+                  color: 'black', 
                 },   
               }}
               optional={
@@ -116,33 +133,21 @@ function VerticalLinearStepper() {
             <StepContent>
               <Typography>{step.description}</Typography>
               <Box sx={{ mb: 2 }}>
-                <Button
-                  variant="contained"
-                  onClick={handleNext}
-                  sx={{ mt: 1, mr: 1 }}
-                >
-                  {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                </Button>
-                <Button
-                  disabled={index === 0}
-                  onClick={handleBack}
-                  sx={{ mt: 1, mr: 1 }}
-                >
+              {activeStep > 0 && (
+                <Button onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
                   Back
                 </Button>
+              )}
+              {activeStep < steps.length - 1 && (
+                <Button onClick={handleNext} variant="contained" sx={{ mt: 1, mr: 1 }}>
+                  Continue
+                </Button>
+              )}
               </Box>
             </StepContent>
           </Step>
         ))}
       </Stepper>
-      {activeStep === steps.length && (
-        <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>All steps completed - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-            Reset
-          </Button>
-        </Paper>
-      )}
     </Box>
   );
 }
